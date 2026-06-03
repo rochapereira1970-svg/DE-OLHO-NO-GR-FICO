665be637833891234ffb10119ffb373b7e5a8ea0
MESTRE DO OVER <rochapereira1970@gmail.com>
2026-06-03 18:23:46 -0300

    Update over15.py

--- robots/over15.py
+++ robots/over15.py
@@ -1,1 +1,24 @@
-# Robô Over 1.5 FT
+from robots.icom import (
+    calcular_icom_over15,
+    classificar_icom
+)
+
+def testar_robo():
+
+    icom = calcular_icom_over15(
+        over15_casa=90,
+        over15_fora=85,
+        media_gols_casa=1.8,
+        media_gols_fora=1.5,
+        h2h_over15=80
+    )
+
+    classificacao = classificar_icom(icom)
+
+    resultado = {
+        "jogo": "Flamengo x Palmeiras",
+        "icom": icom,
+        "classificacao": classificacao
+    }
+
+    return resultado
