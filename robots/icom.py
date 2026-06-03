32c70a9c4446866a914d51ea54f1f8d994bb67f7
MESTRE DO OVER <rochapereira1970@gmail.com>
2026-06-03 18:08:13 -0300

    Update icom.py

--- robots/icom.py
+++ robots/icom.py
@@ -1,1 +1,13 @@
 # ICOM - Índice de Confiança de Oportunidade de Mercado
+
+def classificar_icom(pontos):
+    if pontos >= 95:
+        return "ELITE"
+    elif pontos >= 90:
+        return "PREMIUM"
+    elif pontos >= 85:
+        return "FORTE"
+    elif pontos >= 80:
+        return "BOA"
+    else:
+        return "DESCARTAR"
